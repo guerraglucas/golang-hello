421a76b8f76d73459a9870ca79ac5a6fbc40a3f9
Lucas Guerra <lucas@Zeus-Apple-FVHH9YSQQ6L4.local>
2023-02-23 17:25:42 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nome := "Lucas"
+	idade := 20
+	versao := 1.1
+	fmt.Println("Olá Sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
+}
